f641bf2080caf8e1074921822dc93d01622f5d02
Amit Mathew <amit@amit2.com>
2023-05-02 13:22:42 -0400

    Fix bug with muzzle flashes on missile towers and helicopters

--- entities/shooter_alternating.gd
+++ entities/shooter_alternating.gd
@@ -4,13 +4,14 @@ var _shoot_a := true
 	
 func shoot() -> void:
 	can_shoot = false
-	var _muzzle : Marker2D
+	var muzzle : Marker2D
 	if _shoot_a:
-		_muzzle = gun.get_children()[0]
+		muzzle = gun.get_children()[0]
 		_play_animations("shoot_a")
 	else:
-		_muzzle = gun.get_children()[1]
+		muzzle = gun.get_children()[1]
 		_play_animations("shoot_b")
-	_instantiate_projectile(_muzzle.global_position, targets.front())
+	muzzle_flash.global_position = muzzle.global_position
+	_instantiate_projectile(muzzle.global_position, targets.front())
 	firerate_timer.start(fire_rate)
 	_shoot_a = !_shoot_a
