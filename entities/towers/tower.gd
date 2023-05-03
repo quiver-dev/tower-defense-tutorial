f75618c68ab941298a3fdca7968cf339b56cf132
Amit Mathew <amit@amit2.com>
2023-05-03 15:30:46 -0400

    Add detector circle for towers

--- entities/towers/tower.gd
+++ entities/towers/tower.gd
@@ -6,11 +6,14 @@ signal tower_destroyed
 @export_range(1, 1000) var health: int = 100:
 	set = set_health
 @export var tower_type: String
+@export var detector_color := Color(0, 0, 1.0, 0.09)
 
 var max_health: int
+var _is_mouse_hovering := false
 
 @onready var collision = $CollisionShape2D as CollisionShape2D
 @onready var shooter = $Shooter as Shooter
+@onready var detector_shape = $Shooter/Detector/CollisionShape2D as CollisionShape2D
 @onready var hud := $UI/EntityHUD as EntityHUD
 
 func _ready():
@@ -23,6 +26,10 @@ func _physics_process(delta: float) -> void:
 		shooter._rotate_shooter(delta)
 		if shooter.should_shoot():
 			shooter.shoot()
+			
+func _draw() -> void:
+	if _is_mouse_hovering:
+		draw_circle(Vector2.ZERO, detector_shape.shape.radius, detector_color)
 	
 func set_health(value: int) -> void:
 	health = max(0, value)
@@ -40,6 +47,16 @@ func repair():
 func _on_gun_animation_finished():
 	if shooter.gun.animation == "die":
 		queue_free()
+
+
+func _on_mouse_entered():
+	_is_mouse_hovering = true
+	queue_redraw()
+
+
+func _on_mouse_exited():
+	_is_mouse_hovering = false
+	queue_redraw()
 
 
 func _on_shooter_has_shot(reload_time):
