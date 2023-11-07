9f5cc040da4eca020afd8b10afefc238dd30708d
Amit Mathew <amit@quiver.dev>
2023-11-07 13:15:27 -0500

    Fix camera panning so that camera pans slower when camera is zoomed in

--- maps/camera/camera2d.gd
+++ maps/camera/camera2d.gd
@@ -4,7 +4,7 @@ extends Camera2D
 @export var max_zoom := 1.0
 @export var zoom_rate := 16.0
 @export var zoom_delta := 0.1
-@export var drag_speed := 4.0
+@export var drag_speed := 1.0
 
 @onready var target_zoom: float = zoom.x
 @onready var hud := $HUD as HUD
@@ -16,15 +16,15 @@ func _physics_process(delta: float) -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			target_zoom = min(target_zoom + zoom_delta, max_zoom)
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			target_zoom = max(target_zoom - zoom_delta, min_zoom)
 		if event.pressed:
-			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-				target_zoom = min(target_zoom + zoom_delta, max_zoom)
-			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-				target_zoom = max(target_zoom - zoom_delta, min_zoom)
 			if event.button_index == MOUSE_BUTTON_MIDDLE:
 				Input.set_default_cursor_shape(Input.CURSOR_DRAG)
 		elif event.button_index == MOUSE_BUTTON_MIDDLE:
 			Input.set_default_cursor_shape(Input.CURSOR_ARROW)
 	if event is InputEventMouseMotion and \
 			event.button_mask == MOUSE_BUTTON_MASK_MIDDLE:
-		position -= event.relative * drag_speed * zoom 
+		position -= event.relative * drag_speed / zoom
